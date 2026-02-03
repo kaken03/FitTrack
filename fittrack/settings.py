3a3aeab8ef89f25a8b63bdc64b5f112c9d4b4d80
kaken03 <kentezekielvillahermosa@gmail.com>
2026-02-03 20:44:29 +0800

    final

--- fittrack/settings.py
+++ fittrack/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-y#znwu+9s57k_-#xs=w&xd0lzh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['kaken.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
